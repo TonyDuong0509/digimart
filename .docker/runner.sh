19bb95c1b97e499f1132aec09864b18fa7f7c49f
Francis Duong <128566649+TonyDuong0509@users.noreply.github.com>
2025-05-03 23:53:49 +0700

    Update runner.sh

--- .docker/runner.sh
+++ .docker/runner.sh
@@ -10,15 +10,16 @@ mkdir -p /var/www/html/storage/logs
 chown -R www-data:www-data /var/www/html
 
 # Chạy composer migrate và chạy optimize
-cd /var/www/html && COMPOSER_PROCESS_TIMEOUT=0 composer install
-cd /var/www/html && php artisan key:generate
-cd /var/www/html && php artisan storage:link
-cd /var/www/html && php artisan cache:clear && php artisan view:clear && php artisan optimize && php artisan queue:restart
-cd /var/www/html && composer require laravel/breeze --dev
-cd /var/www/html && php artisan breeze:install
-cd /var/www/html && php artisan migrate
+cd /var/www/html
+COMPOSER_PROCESS_TIMEOUT=0 composer install
+composer require laravel/breeze --dev
+php artisan key:generate
+php artisan storage:link
+php artisan breeze:install blade
+php artisan migrate
+php artisan cache:clear && php artisan view:clear && php artisan optimize && php artisan queue:restart
 
 # Vite install
-cd /var/www/html && npm install && npm run build
+npm install && npm run build
 
 supervisord -c /etc/supervisor/supervisord.conf
