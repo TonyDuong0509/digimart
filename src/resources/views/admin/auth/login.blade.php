f9215df80a3cf0efd4607f99a4d8c8db1617e750
Francis Duong <duonganhhao4751@gmail.com>
2025-05-05 15:56:04 +0700

    fix: delete h1 in login admin.

--- src/resources/views/admin/auth/login.blade.php
+++ src/resources/views/admin/auth/login.blade.php
@@ -1,7 +1,6 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-    <h1>Admin Login</h1>
     <form method="POST" action="{{ route('admin.login') }}">
         @csrf
 
